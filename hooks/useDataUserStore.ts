595b188b2769847f99f1accc9838b0f4f4894f8c
JasonEvan <7ason3van@gmail.com>
2025-03-12 09:22:35 +0700

    bug fixing dengan menambah middleware persist zustand

--- hooks/useDataUserStore.ts
+++ hooks/useDataUserStore.ts
@@ -1,19 +1,25 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-interface userData {
+interface UserData {
   email: string;
   isAdmin: boolean;
   setEmail: (email: string) => void;
   setIsAdmin: (isAdmin: boolean) => void;
 }
 
-export const useDataUser = create<userData>((set) => ({
-  email: "",
-  isAdmin: false,
-  setEmail: (email) => {
-    set({ email: email });
-  },
-  setIsAdmin: (isAdmin) => {
-    set({ isAdmin: isAdmin });
-  },
-}));
+export const useDataUser = create<UserData>()(
+  persist(
+    (set) => ({
+      email: "",
+      isAdmin: false,
+      setEmail: (email) => {
+        set({ email: email });
+      },
+      setIsAdmin: (isAdmin) => {
+        set({ isAdmin: isAdmin });
+      },
+    }),
+    { name: "auth-storage" }
+  )
+);
